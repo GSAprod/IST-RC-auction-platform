ad0d826810ba0e860df7c7f9689078534f27bced
GSAprod <92989496+GSAprod@users.noreply.github.com>
2023-11-26 10:40:18 +0000

    Add prompt verification on login command

--- auction_client.c
+++ auction_client.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <ctype.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -35,6 +36,53 @@ int promptToArgsList(char* prompt, char prompt_args[][128]) {
     }
     return argCount;
 }
+
+/***
+ * Login to the auction server using the credentials provided.
+ * The function sends a message to the AS using the UDP protocol with the
+ * user credentials provided, and displays the result of the request, which
+ * can be a successful login, a failed login, and a new user being generated.
+ * 
+ * @param arg_count The number of arguments given to the command
+ * @param args A list of arguments given to the function. The list should have
+ * 3 arguments: the name of the command, the istID and a password.
+*/
+void clientLogin(int arg_count, char args[][128]) {
+    int scanf_success;
+    int istId;
+    int passwdLen;
+
+    // Argument verification
+    if(arg_count != 3) {
+        printf("Login: Wrong arguments given.\nlogin UID password\n");
+        return;
+    }
+
+    // Verify if the istID is a 6 digit number
+    scanf_success = sscanf(args[1], "%d", &istId);
+    if (scanf_success != 1 || strlen(args[1]) != 6) {
+        printf("Login: Wrong arguments given.\nlogin UID password\n");
+        return;
+    }
+    
+    // Verify if the student password is an 8 character alphanumeric string
+    passwdLen = strlen(args[2]);
+    if (passwdLen != 8) {
+        printf("Login: Wrong arguments given.\nlogin UID password\n");
+        return;
+    }
+    for(int i = 0; i < passwdLen; i++) {
+        char c = args[2][i];
+        if (!isalpha(c) && !isdigit(c)) {
+            printf("Login: Wrong arguments given.\nlogin UID password\n");
+            return;
+        }
+    }
+
+    //* Now connect to the server and send the credentials.
+    //* Receive the arguments and act accordingly
+}
+
 int main(int argc, char *argv[]) {
     char prompt[512];
     char prompt_args[16][128];
@@ -53,7 +101,7 @@ int main(int argc, char *argv[]) {
     //? Do not forget to check if the arguments (and count) are correct
     //! This if chain supposedly works, but it hasn't been tested (the rest was)
     if(!strcmp(prompt_args[0], "login")) {
-        // TODO Login function
+        clientLogin(prompt_args_count, prompt_args);
     } else if (!strcmp(prompt_args[0], "logout")) {
         // TODO Logout function
     } else if (!strcmp(prompt_args[0], "unregister")) {
